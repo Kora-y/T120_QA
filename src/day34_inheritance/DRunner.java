cbcd226e8892b38963906c41f9dd03db5f5a8f4f
kora-y <korayyyygul20003@gmail.com>
2023-08-18 22:46:39 +0200

    Inheritance lesson 34

--- src/day34_inheritance/DRunner.java
+++ src/day34_inheritance/DRunner.java
@@ -9,5 +9,18 @@ public class DRunner {
         System.out.println(corolla.model);  // C Corolla
         System.out.println(corolla.aku); // Ince aku
         System.out.println(corolla.sanziman); //cvt
+
+        // Toyota class'indaki ozellikler
+        System.out.println(corolla.marka); // Toyota
+        System.out.println(corolla.lastik); // Tum toyota araclar pirelli lastik kullanir
+        System.out.println(corolla.guvenlik);// Tum Toyota araclar extra guvenlik icerir
+        System.out.println(corolla.fren); // Toyota araclar abs fren sistemi kullanir
+
+        // Araba class'indan aldigi ozellikler
+        System.out.println(corolla.yakit); // yakit turu belirtilmedi
+        corolla.yakit ="Benzin";
+        System.out.println(corolla.yakit); // Benzin
+        System.out.println(corolla.yil); // 1900
+        System.out.println(corolla.renk); // renk belirtilmedi
     }
 }
---
+++ src/day34_inheritance/EHayvanlar.java
@@ -0,0 +1,30 @@
+package day34_inheritance;
+
+public class EHayvanlar {
+    // Bir class olusturulurken
+    // ilerde child class'lar olabilir diye dusunuyorsaniz
+    // sadece child class'larin ulasmasini istediginiz method'lari
+    // protected access modifier ile olusturabilirsiniz
+    protected void hareket(){
+        System.out.println("hareket ederler");
+    }
+
+    protected void solunum(){
+        System.out.println("nefes alirlar");
+    }
+
+    protected void beslenme(){
+
+        System.out.println("beslenirler");
+    }
+
+    protected void cogalma(){
+
+        System.out.println("cogalirlar");
+    }
+
+    protected void omur(){
+
+        System.out.println("yasar ve olurler");
+    }
+}
