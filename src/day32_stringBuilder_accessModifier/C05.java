0aef81dfa5b92d458f1d647e901ea4fd1da89526
kora-y <korayyyygul20003@gmail.com>
2023-08-18 11:31:41 +0200

    Started to learn QA

--- src/day32_stringBuilder_accessModifier/C05.java
+++ src/day32_stringBuilder_accessModifier/C05.java
@@ -1,5 +1,8 @@
 package day32_stringBuilder_accessModifier;
 
 public class C05 {
-
+    private int privateSayi;
+    int defaultSayi;
+    protected int protectedSayi;
+    public int publicSayi;
 }
