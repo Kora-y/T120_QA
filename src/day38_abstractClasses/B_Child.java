728e4e00432a63a2be09395c84450b51e2ea59b9
kora-y <korayyyygul20003@gmail.com>
2023-08-27 10:03:44 +0200

    Day38 first commit

---
+++ src/day38_abstractClasses/B_Child.java
@@ -0,0 +1,18 @@
+package day38_abstractClasses;
+
+public class B_Child extends A_KuralciParent{
+    @Override
+    public void method3() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+
+    @Override
+    public void method5() {
+
+    }
+}
