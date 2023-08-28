3ded3ad86e1b86d89fdbc4c3a87a6da8a0cd4326
kora-y <korayyyygul20003@gmail.com>
2023-08-28 20:58:11 +0200

    First Upload of the Day

---
+++ src/day40_interfaces/C01_ChildOfInterfaces.java
@@ -0,0 +1,37 @@
+package day40_interfaces;
+
+import day39_interfaces.I02_Interfaces;
+
+
+public class C01_ChildOfInterfaces implements I01_Interfaces,I02_Interfaces {
+
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public int method2() {
+        return 0;
+    }
+
+    @Override
+    public int method11() {
+        return 0;
+    }
+
+    @Override
+    public String method3() {
+        return null;
+    }
+
+    @Override
+    public void method44() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+}
