47bd1df18051f1a17d0969b96021746f664c7349
kora-y <korayyyygul20003@gmail.com>
2023-09-06 19:24:58 +0200

    06.09.2023

---
+++ src/solvedQuestions/P01/Sekil.java
@@ -0,0 +1,20 @@
+package solvedQuestions.P01;
+
+public class Sekil {
+    double uzunKenar;
+
+    double kisaKenar;
+
+    double yariCap;
+
+
+    public Sekil(double uzunKenar, double kisaKenar) {
+        this.uzunKenar = uzunKenar;
+        this.kisaKenar = kisaKenar;
+    }
+
+    public Sekil(double yariCap) {
+        this.yariCap = yariCap;
+    }
+
+}
