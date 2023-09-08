ebb87d98e63b57bc370116017338562e252dd88e
kora-y <korayyyygul20003@gmail.com>
2023-09-08 23:46:39 +0200

    23:46

---
+++ src/day43_collections/C02_LinkedList_Deque.java
@@ -0,0 +1,13 @@
+package day43_collections;
+
+public class C02_LinkedList_Deque {
+    public static void main(String[] args) {
+
+         /*
+            Deque : double ended queue demektir. Cift basli kuyruk
+            iki bastan da islem yapildigi icin pekcok method'un first ve last seklinde 2 versiyonu olur
+
+         */
+
+    }
+}
