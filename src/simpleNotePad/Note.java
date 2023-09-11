816696bd9f32fe06a52bbbe0827e7a83232376b0
kora-y <korayyyygul20003@gmail.com>
2023-09-11 17:15:32 +0200

    11.09.2023

--- src/simpleNotePad/Note.java
+++ src/simpleNotePad/Note.java
@@ -3,16 +3,16 @@ package simpleNotePad;
 import java.util.ArrayList;
 
 public class Note {
-    private String title ;
+    private String title;
     private String content;
 
-    private String deadline ;
+    private String deadline;
 
     public Note() {
 
     }
 
-    public Note(String title,String content, String deadline) {
+    public Note(String title, String content, String deadline) {
         this.title = title;
         this.content = content;
         this.deadline = deadline;
@@ -32,10 +32,10 @@ public class Note {
 
     public void getInfo(ArrayList<Note> list) {
         for (Note note : list) {
-            System.out.println("Title: " + note.getTitle());
-            System.out.println("Content: " + note.getContent());
-            System.out.println("Deadline: " + note.getDeadline());
-            System.out.println("-------------------");
+            System.out.println(" Title : " + note.getTitle());
+            System.out.println(" Content : " + note.getContent());
+            System.out.println(" Deadline : " + note.getDeadline());
+            System.out.println("************************************");
         }
     }
 }
